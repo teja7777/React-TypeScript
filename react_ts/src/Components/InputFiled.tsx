9fd53ff060ca22d9e5762dc3b82eded4a0cfa768
teja7777 <52334571+teja7777@users.noreply.github.com>
2024-06-13 19:16:44 +0530

    feat:implement InputFiled component

--- react_ts/src/Components/InputFiled.tsx
+++ react_ts/src/Components/InputFiled.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import "./styles.css"
 const InputFiled = () => {
   return (
-    // <form className='input'>
-    //   <input type='input' placeholder='enter to do' className='input__box' />
-    //   <button className='input_button' type='submit'>GO</button>
-    // </form>
+    <form className='input'>
+      <input type='input' placeholder='enter to do' className='input__box' />
+      <button className='input_button' type='submit'>GO</button>
+    </form>
   )
 }
 
