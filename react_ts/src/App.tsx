f0d6b2069706b68c150c9268784773e7d8ff6032
gleecus <teja.ulisetti@gleecus.com>
2024-06-13 19:08:47 +0530

    feat:add inout field

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputFiled from './Components/InputFiled';
 
-const  App : React.FC = () => {
+const App: React.FC = () => {
   return (
     <div className="App">
+      <span className='heading'>TASKIFY </span>
+      <InputFiled />
     </div>
   );
 }
---
+++ react_ts/src/Components/InputFiled.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css"
+const InputFiled = () => {
+  return (
+    // <form className='input'>
+    //   <input type='input' placeholder='enter to do' className='input__box' />
+    //   <button className='input_button' type='submit'>GO</button>
+    // </form>
+  )
+}
+
+export default InputFiled
