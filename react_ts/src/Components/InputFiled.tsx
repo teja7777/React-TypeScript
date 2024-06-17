270a187983e13593c9bb79f680d7538e825aecef
teja7777 <52334571+teja7777@users.noreply.github.com>
2024-06-17 16:52:06 +0530

    refactor:commenting and uncommenting unnecesaary changes.

--- react_ts/src/Components/InputFiled.tsx
+++ react_ts/src/Components/InputFiled.tsx
@@ -1,19 +1,19 @@
-import React, { SyntheticEvent } from 'react'
-import "./styles.css"
+// import React, { SyntheticEvent } from 'react'
+// import "./styles.css"
 
-interface Props {
-  todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd: (e:SyntheticEvent) => void;
-}
+// interface Props {
+//   todo: string;
+//   setTodo: React.Dispatch<React.SetStateAction<string>>;
+//   handleAdd: (e:SyntheticEvent) => void;
+// }
 
-const InputFiled: React.FC<Props> = ({ todo, setTodo,handleAdd }) => {
-  return (
-    <form className='input' onSubmit={handleAdd}>
-      <input type='input' value={todo} onChange={(e) => { setTodo(e.target.value) }} placeholder='enter to do' className='input__box' />
-      <button className='input_button' type='submit'>GO</button>
-    </form>
-  )
-}
+// const InputFiled: React.FC<Props> = ({ todo, setTodo,handleAdd }) => {
+//   return (
+//     <form className='input' onSubmit={handleAdd}>
+//       <input type='input' value={todo} onChange={(e) => { setTodo(e.target.value) }} placeholder='enter to do' className='input__box' />
+//       <button className='input_button' type='submit'>GO</button>
+//     </form>
+//   )
+// }
 
-export default InputFiled
+// export default InputFiled
