49ce9e5b9cb0ad2380288798ce31974cd0a8d18d
teja7777 <52334571+teja7777@users.noreply.github.com>
2024-06-19 21:06:42 +0530

    update input field to resolve issues

--- react_ts/src/Components/InputFiled.tsx
+++ react_ts/src/Components/InputFiled.tsx
@@ -1,22 +1,22 @@
-import React, { SyntheticEvent, useRef } from 'react'
-import "./styles.css"
+// import React, { SyntheticEvent, useRef } from 'react'
+// import "./styles.css"
 
-interface Props {
-  todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd: (e: SyntheticEvent) => void;
-}
-const InputFiled: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  return (
-    <form className='input' onSubmit={(e)=>{
-      handleAdd(e)
-      inputRef.current?.blur();
-    }}>
-      <input ref={inputRef} type='input' value={todo} onChange={(e) => { setTodo(e.target.value) }} placeholder='enter to do' className='input__box' />
-      <button className='input_button' type='submit'>GO</button>
-    </form>
-  )
-}
+// interface Props {
+//   todo: string;
+//   setTodo: React.Dispatch<React.SetStateAction<string>>;
+//   handleAdd: (e: SyntheticEvent) => void;
+// }
+// const InputFiled: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+//   const inputRef = useRef<HTMLInputElement>(null);
+//   return (
+//     <form className='input' onSubmit={(e)=>{
+//       handleAdd(e)
+//       inputRef.current?.blur();
+//     }}>
+//       <input ref={inputRef} type='input' value={todo} onChange={(e) => { setTodo(e.target.value) }} placeholder='enter to do' className='input__box' />
+//       <button className='input_button' type='submit'>GO</button>
+//     </form>
+//   )
+// }
 
-export default InputFiled
+// export default InputFiled
